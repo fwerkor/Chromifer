52f511bfac0b58b94d3b2fc81a7407b4155133c2
Cao Yuhang <caoyuhang@fwerkor.com>
2026-08-05 16:54:44 +0000

    chore: remove generated example artifacts

--- examples/rust-bridge/target/debug/deps/chromifer_rust_bridge_example-d4de1a480a11ccd6.d
+++
@@ -1,6 +0,0 @@
-/workspace/Chromifer/examples/rust-bridge/target/debug/deps/chromifer_rust_bridge_example-d4de1a480a11ccd6.d: src/lib.rs src/parser.rs
-
-/workspace/Chromifer/examples/rust-bridge/target/debug/deps/libchromifer_rust_bridge_example-d4de1a480a11ccd6.rmeta: src/lib.rs src/parser.rs
-
-src/lib.rs:
-src/parser.rs:
